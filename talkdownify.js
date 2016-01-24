b93e9157b6d4b95e9e2ac89ec0fa029fdfb14a81
anglessteven <angles.steven@gmail.com>
2016-01-24 17:36:13 -0500

    more layout stuff

--- talkdownify.js
+++ talkdownify.js
@@ -9,10 +9,24 @@
     function TalkdownifyController($log) {
         var talkdownifyController = this;
 
+        talkdownifyController.keywords = [
+            { word: "Obviously", checked: true },
+            { word: "Clearly", checked: true },
+            { word: "Plainly", checked: true },
+            { word: "Of course", checked: true }
+        ];
+
         talkdownifyController.talkdownify = talkdownify;
+        talkdownifyController.addKeyword = addKeyword;
 
         function talkdownify() {
             $log.debug("text is: ", talkdownifyController.text);
+        }
+
+        function addKeyword() {
+            $log.debug("keywordText: ", talkdownifyController.keywordText);
+            talkdownifyController.keywords.push({ word: talkdownifyController.keywordText, checked: true });
+            talkdownifyController.keywordText = '';
         }
     }
 }());
